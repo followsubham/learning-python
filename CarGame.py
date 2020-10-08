48de3781c0d22ccbae444084790f222be18ba875
Subham Das <6717452+followsubham@users.noreply.github.com>
2020-10-08 18:50:52 +0530

    FewChanges

--- CarGame.py
+++ CarGame.py
@@ -1,11 +1,19 @@
 #CAR GAME
 command=""
+started=False
 while True:
     command=input (">").lower()
     if command=="start":
-        print("Car Started...")
+        if started:
+            print("Car already started")
+        else started=True
+            print("Car Started...")
     elif command=="stop":
+        if not started
+            print ("Car already stopped")
+        else
         print("Car Stopped")
+        started=False
     elif command=="help":
         print("""
 Start -  To Start the car
