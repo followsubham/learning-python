ff4732b35a855b0d3cf7a9428dd9fdbb5a667bc7
Subham Das <6717452+followsubham@users.noreply.github.com>
2020-10-28 10:45:50 +0530

    NewPackageAdded

--- microblog/myblog/views.py
+++ microblog/myblog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts=[{
+    'author': 'Subham Das',
+    'title': 'Blog 1',
+    'content': 'First Post Content',
+    'published_on': '25-10-2020',
+}, {
+    'author': 'Baba',
+    'title': 'Blog 2',
+    'content': 'Second Post Content',
+    'published_on': '25-10-2020',
+}, {
+    'author': 'Ma',
+    'title': 'Blog 3',
+    'content': 'Third Post Content',
+    'published_on': '25-10-2020',
+},
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Subham Das, did it!!!</h1>')
+    context={'posts':posts}
+    return render(request, 'myblog/home-index.html', context)
 
 # Create your views here.
